9354192ab46bd2cc23ed4dd301502aec2773ae39
Lilia <geertszelia@gmail.com>
2016-10-25 10:46:15 +0200

    Début de l'étape 2 faite

--- js/app.js
+++ js/app.js
@@ -1,19 +1,41 @@
 (function(){
 	"use strict";
 	var app = {
-		url:'http://192.168.1.21:1337/alice.md',
+		urlAlice:'http://192.168.1.21:1337/alice.md',
+		urlMenu: 'http://192.168.1.21:1337/menu.json',
 
 		init:function(){
-			$.ajax(this.url)
-			.done(this.ajaxDone)
+			this.ajaxAlice();
+			this.ajaxMenu();
+		},
+
+		ajaxAlice: function(){
+			$.ajax(this.urlAlice)
+			.done(this.ajaxDoneAlice)
 			.fail(this.ajaxFail)
 			.always(this.ajaxAlways);
 		},
 
-		ajaxDone: function(alice){
+		ajaxMenu: function(){
+			$.ajax(this.urlMenu)
+			.done(this.ajaxDoneMenu)
+			.fail(this.ajaxFail)
+			.always(this.ajaxAlways);
+		},
+
+		ajaxDoneAlice: function(alice){
 			var converter = new showdown.Converter();
 			var html = converter.makeHtml(alice);
 			$('#md').text(html);			
+		},
+
+		ajaxDoneMenu: function(menu){
+			var allMenu = menu.menu;
+			for(var i = 0; i < allMenu.length; i++){
+				var converter = new showdown.Converter();
+				var html = converter.makeHtml(allMenu[i]);
+				$('#md').text(html);
+			}
 		},
 
 		ajaxFail: function(){
