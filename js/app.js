b1134dab2510ed0dca2e6d8488f59b718bad6174
Lilia <geertszelia@gmail.com>
2016-10-25 09:52:08 +0200

    Ajout de l'étape 1 fini : récupérer le contenu de alice.md et le convertir en html

--- js/app.js
+++ js/app.js
@@ -2,7 +2,26 @@
 	"use strict";
 	var app = {
 		init:function(){
-			// let's go
+			var url = 'http://192.168.1.21:1337/alice.md'
+			$.ajax(url)
+			.done(this.ajaxDone)
+			.fail(this.ajaxFail)
+			.always(this.ajaxAlways);
+		},
+
+		ajaxDone: function(alice){
+			var alice = alice;
+			var converter = new showdown.Converter();
+			text = $('#md').text(alice);
+			html = converter.makeHTML(text);			
+		},
+
+		ajaxFail: function(){
+			console.log('fail!');
+		},
+
+		ajaxAlways: function(){
+			console.log('always');
 		},
 	};
 
